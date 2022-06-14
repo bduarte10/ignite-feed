e3bb977a33895b832928953736143628a857179b
Bruno Duarte <duartebruno2208@gmail.com>
2022-06-13 21:07:31 -0300

    sidebar finalizada

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,11 +9,15 @@ export function Sidebar() {
 				src="https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
 			/>
 			<div className={styles.profile}>
+				<img className={styles.avatar} src="https://github.com/bduarte10.png" />
 				<strong>Bruno Duarte</strong>
 				<span>Web Developer</span>
 			</div>
 			<footer>
-				<a href="#">Editar seu perfil</a>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar seu perfil
+				</a>
 			</footer>
 		</aside>
 	);
