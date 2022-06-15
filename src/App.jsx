4d06286882663ab222f9dd0d1d73df2fa7c45bb3
Bruno Duarte <duartebruno2208@gmail.com>
2022-06-15 16:57:43 -0300

    comment component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import { Header } from './components/Header';
 import { Post } from './components/Post';
-import './global.css';
-import styles from './App.module.css';
 import { Sidebar } from './components/Sidebar';
 
+import styles from './App.module.css';
+
+import './global.css';
+
 export function App() {
-	return (
-		<div>
-			<Header />
-			<div className={styles.wrapper}>
-				<Sidebar />
-				<main>
-					<Post
-						author="Bruno Duarte"
-						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis ipsum rem voluptatem nobis earum obcaecati corrupti quae voluptas? Sed voluptas "
-					/>
-				</main>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Bruno Duarte"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis ipsum rem voluptatem nobis earum obcaecati corrupti quae voluptas? Sed voluptas "
+          />
+          <Post
+            author="Bruno Duarte"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis ipsum rem voluptatem nobis earum obcaecati corrupti quae voluptas? Sed voluptas "
+          />
+        </main>
+      </div>
+    </div>
+  );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,38 @@
+import styles from './Post.module.css';
+
 export function Post() {
-	return <h1>Hello word</h1>;
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.author}
+            src="https://github.com/bduarte10.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Bruno Duarte</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="15 de Junho às 08:00h" datetime="2022-06-15 08:00">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifólio. É um projeto que
+          fiz no NLW Return, evento da Rocketseat. O nome do projeto é
+          DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
 }
