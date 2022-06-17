4790ec9392243d7046ea7ec8cb5f95379c653c3b
Bruno Duarte <duartebruno2208@gmail.com>
2022-06-17 16:49:18 -0300

    avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src=" https://github.com/bduarte10.png" />
+      <Avatar hasBorder={false} src=" https://github.com/bduarte10.png" />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
@@ -18,7 +19,7 @@ export function Comment() {
               </time>
             </div>
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p>Muito bom, Parabéns!!</p>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.author}
-            src="https://github.com/bduarte10.png"
-          />
+          <Avatar src="https://github.com/bduarte10.png" />
           <div className={styles.authorInfo}>
             <strong>Bruno Duarte</strong>
             <span>Web Developer</span>
