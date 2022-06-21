d69616a88b4b2c050b8f0bc1853748271dbfa268
Bruno Duarte <duartebruno2208@gmail.com>
2022-06-20 21:36:02 -0300

    like counter add

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,19 @@
+import { set } from 'date-fns/esm';
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+  function handleLikedComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
   }
 
   return (
@@ -29,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikedComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
